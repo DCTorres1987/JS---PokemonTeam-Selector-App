c5fd0b561bc0be3d19c63015ad9e16bf9f92bd69
DCTorres87 <cruz_danielle_torres@yahoo.com>
2020-08-24 20:57:05 -0400

    update

---
+++ pokemonteam-selectorapp/pokemonteam-selector-api/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pokemon.create(name: "bulbasaur", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png")
+Pokemon.create(name: "ditto", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png")
